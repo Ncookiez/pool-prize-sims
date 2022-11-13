f074d2f2c753e7b0d494e11c00141582cb1e513c
Ncookiez <ncukiert@gmail.com>
2022-11-13 11:28:15 -0300

    Updated Defaults

--- src/lib/defaults.ts
+++ src/lib/defaults.ts
@@ -5,10 +5,10 @@ import type { Chain, PrizeDistribution } from '$lib/types';
 /* ========================================================================================================================================================================= */
 
 // Default TVL:
-export const defaultTVL: number = 50_000_000;
+export const defaultTVL: number = 40_000_000;
 
 // Default Yield Rate:
-export const defaultRate: number = 2;
+export const defaultRate: number = 0.5;
 
 // Default Chain:
 export const defaultChain: Chain = 'eth';
@@ -18,10 +18,10 @@ export const defaultChain: Chain = 'eth';
 // Default Distributions:
 export const defaultDistributions: Record<Chain, PrizeDistribution> = {
   eth: {
-    1: 1000,
+    1: 200,
     2: null,
     4: null,
-    8: 400,
+    8: 164,
     16: null,
     32: null,
     64: null,
@@ -34,48 +34,48 @@ export const defaultDistributions: Record<Chain, PrizeDistribution> = {
     8192: null
   },
   poly: {
-    1: 1000,
-    2: null,
-    4: 8,
-    8: null,
+    1: 200,
+    2: 20,
+    4: 20,
+    8: 1,
     16: null,
-    32: 5,
-    64: 5,
-    128: 5,
+    32: 1,
+    64: null,
+    128: 1,
     256: null,
-    512: null,
-    1024: null,
-    2048: 1,
+    512: 1,
+    1024: 0.5,
+    2048: null,
     4096: null,
     8192: null
   },
   avax: {
-    1: 1000,
+    1: 200,
     2: null,
-    4: null,
-    8: null,
-    16: null,
-    32: 20,
-    64: null,
+    4: 50,
+    8: 5,
+    16: 5,
+    32: 5,
+    64: 5,
     128: null,
-    256: null,
-    512: 5,
+    256: 2,
+    512: null,
     1024: null,
     2048: null,
     4096: null,
     8192: null
   },
   op: {
-    1: 1000,
+    1: 200,
     2: null,
-    4: null,
-    8: null,
-    16: null,
-    32: 20,
-    64: null,
+    4: 50,
+    8: 5,
+    16: 5,
+    32: 5,
+    64: 5,
     128: null,
-    256: null,
-    512: 5,
+    256: 2,
+    512: null,
     1024: null,
     2048: null,
     4096: null,
